154ace984c0ad180ad8e0cd0199e51c3a8907f80
Pratima Rawat <pritirawat2048@gmail.com>
2025-03-28 16:33:59 +0530

    Fixed static file configuration

--- Blood_Bank/settings.py
+++ Blood_Bank/settings.py
@@ -53,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'Blood_Bank.urls'
 
@@ -121,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blood_app', 'static'),
 ]
