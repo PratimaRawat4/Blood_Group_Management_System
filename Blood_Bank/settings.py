d02fc6aef39eb0c5fd987fd7632805f89f5e5800
Pratima Rawat <pritirawat2048@gmail.com>
2025-03-28 16:12:22 +0530

    Updated settings.py with new changes

--- Blood_Bank/settings.py
+++ Blood_Bank/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-8b255$dr9ub&ehik^1pysy2@1n7$1km@42y^(@clv#z+$gl36%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = ['blood-group-management-system.onrender.com', 'localhost']
+ALLOWED_HOSTS = ["blood-group-management-system.onrender.com", "localhost", "127.0.0.1"]
+
+
+
 
 
 
